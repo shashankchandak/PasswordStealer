3997303b389bbf8cb7d86eafc070ddc11602a736
Shashank Chandak <chandak.shashank16@gmail.com>
2019-06-14 21:22:49 +0530

    db connected

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/storePass',function (req,res) {
   console.log(req.body.data);
+  
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017/passwords";
+  
+  MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("passwords");
+  //change hardcoded to user name
+  dbo.collection("shashank").insertOne(req.body.data, function(err, res) {
+      if (err) throw err;
+      db.close();
+    });
+  });
 
   res.send("1");
 });
